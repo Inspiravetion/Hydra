0d45cbf99444b63e5fe840f686d3f8a914eb515e
Inspiravetion <charlie.lipford.code@gmail.com>
2014-11-18 10:00:02 -0800

    added specific playground folders for each idea i need to flush out

--- Compiler/playground/vtables/src/main.rs
+++ Compiler/playground/vtables/src/main.rs
@@ -4,9 +4,11 @@
 extern crate phf_mac;
 extern crate phf;
 
-use phf::PhfMap;
+use phf::Map;
 
-static KEYWORDS: PhfMap<&'static str, int> = phf_map! {
+type HyFunc = Box<Fn<(int, f64), int>>;
+
+static KEYWORDS: Map<&'static str, int> = phf_map! {
     "loop"     => 0,
     "continue" => 1,
     "break"    => 2,
@@ -16,10 +18,20 @@ static KEYWORDS: PhfMap<&'static str, int> = phf_map! {
     "ASDFASDF" => 6
 };
 
+static FUNCS : Map<&'static str, HyFunc> = phf_map! {
+    "hello"    => hello
+};
+
+fn hello(i : int, f : f64) -> int {
+    println!("world");
+    1
+}
+
 // pub fn parse_keyword(keyword: &str) -> Option<int> {
 //     KEYWORDS.find_equiv(keyword).map(|t| t.clone())
 // }
 
 pub fn main() {
-    println!("should be 3 : {}", KEYWORDS.find_equiv("fn"));
+    println!("should be 3 : {}", KEYWORDS.get(&"fn"));
+    println!("{}", *(FUNCS.get(&"hello").unwrap())(1, 2f64));
 }
